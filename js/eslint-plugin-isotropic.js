bad0916dcd50d3139289a157d376c395f35f4eae
Steven Olmsted <steven.olmsted@ibigroup.com>
2018-11-25 11:50:34 -0500

    Update dependencies

--- js/eslint-plugin-isotropic.js
+++ js/eslint-plugin-isotropic.js
@@ -1,11 +1,23 @@
-import * as _eslintComments from 'eslint-plugin-eslint-comments';
-import * as _isotropic from 'eslint-config-isotropic';
+import * as _eslintConfigIsotropic from 'eslint-config-isotropic';
+import * as _eslintPluginEslintComments from 'eslint-plugin-eslint-comments';
 import _sortKeys from './sort-keys.js';
 
-_isotropic = Object.assign({}, _isotropic);
-_isotropic.rules = Object.assign(_isotropic.rules, {
+const configs = {
+        isotropic: {
+            ..._eslintConfigIsotropic
+        }
+    },
+    rules = {
+        ..._eslintPluginEslintComments.rules,
+        'sort-keys': _sortKeys
+    };
+
+configs.isotropic.rules = {
+    ...configs.isotropic.rules,
     'isotropic/disable-enable-pair': 'warn',
+    'isotropic/no-aggregating-enable': 'error',
     'isotropic/no-duplicate-disable': 'error',
+    'isotropic/no-restricted-disable': 'off',
     'isotropic/no-unlimited-disable': 'error',
     'isotropic/no-unused-disable': 'error',
     'isotropic/no-unused-enable': 'error',
@@ -21,15 +33,7 @@ _isotropic.rules = Object.assign(_isotropic.rules, {
             }
         }
     ]
-});
-
-const configs = {
-        isotropic: _isotropic
-    },
-    rules = {
-        ..._eslintComments.rules,
-        'sort-keys': _sortKeys
-    };
+};
 
 export {
     configs,
